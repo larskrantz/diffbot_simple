c8a3a2bc4ba5759a543eb02efcc398940e7de726
Lars Krantz <lars.krantz@alaz.se>
2014-02-27 23:32:23 +0100

    Mod: bulk_factory, handling of single job

--- lib/diffbot_simple/v2/bulk_factory.rb
+++ lib/diffbot_simple/v2/bulk_factory.rb
@@ -6,7 +6,16 @@ module DiffbotSimple::V2
 			@api = :bulk
 		end
 		def all
-			execute_call()[:jobs].map { |e| Bulk.new e.merge(bulk_factory: self) }
+			execute_call()[:jobs].map { |e| create_bulk e  }
+		end
+		def single name: nil, **options
+			response = execute_call options.merge(name: name)
+			job = response[:jobs].first
+			create_bulk job
+		end
+		private
+		def create_bulk **parameters
+			Bulk.new parameters.merge(bulk_factory: self)
 		end
 	end
 end
