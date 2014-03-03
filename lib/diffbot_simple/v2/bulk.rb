7e80e63b92b4501625bb588f10cd0b38f8eaf03b
Lars Krantz <lars.krantz@alaz.se>
2014-03-03 16:54:12 +0100

    Add: Behavior to bulk class, pause, delete, unpause and generic setting of properties

--- lib/diffbot_simple/v2/bulk.rb
+++ lib/diffbot_simple/v2/bulk.rb
@@ -1,8 +1,34 @@
 module DiffbotSimple::V2
 	class Bulk
-		attr_reader :parameters
-		def initialize  **parameters
+		attr_reader :parameters, :name
+		def initialize bulk_api: nil, name: nil, **parameters
+			@bulk_api = bulk_api
+			@name = name
 			@parameters = parameters
+		end
+		def pause
+			send_to_api pause: 1
+		end
+		def unpause
+			send_to_api pause: 0
+		end
+		def delete!
+			send_to_api delete: 1
+		end
+		def method_missing property, *args
+			super if args.empty?
+			h = {}
+			property = property.to_s.gsub /\=$/,""
+			h[property.to_sym] = args.join(",")
+			send_to_api h
+		end
+		private
+		attr_reader :bulk_api
+		def send_to_api **options
+			params = options.merge({name: name})
+			@parameters = bulk_api.single params
+			@parameters.delete :name
+			self
 		end
 	end
 end
---
+++ spec/v2/bulk_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+module DiffbotSimple::V2
+	describe Bulk do
+		let(:bulk_api) { double("bulk_api") }
+		let(:name) { "test" }
+		let(:subject) { Bulk.new bulk_api: bulk_api, name: name }
+		context "when pausing" do
+			it "should send pause = 1 to bulk api" do
+				expect(bulk_api).to receive(:single).with(name: name, pause: 1).and_return({})
+				subject.pause
+			end
+		end
+		context "when unpausing" do
+			it "should send pause = 0 to bulk api" do
+				expect(bulk_api).to receive(:single).with(name: name, pause: 0).and_return({})
+				subject.unpause
+			end
+		end
+		context "when deleting" do
+			it "should send delete = 0 to bulk api" do
+				expect(bulk_api).to receive(:single).with(name: name, delete: 1).and_return({})
+				subject.delete!
+			end
+		end
+		context "when updating general parameters" do
+			it "should send these to bulk api" do
+				expect(bulk_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
+				subject.notifyEmail "foo@b.ar"
+			end
+			it "should even do it as an setter" do
+				expect(bulk_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
+				subject.notifyEmail = "foo@b.ar"
+			end
+		end
+	end
+end
