a12aec79f3073eb7ec5f89f456a6ed71ef9c514d
Lars Krantz <lars.krantz@alaz.se>
2014-03-05 16:49:44 +0100

    Mod: renamed crawl_api parameter to more correct crawlbot_api

--- lib/diffbot_simple/v2/crawl.rb
+++ lib/diffbot_simple/v2/crawl.rb
@@ -1,8 +1,8 @@
 module DiffbotSimple::V2
 	class Crawl
 		attr_reader :parameters, :name
-		def initialize crawl_api: nil, name: nil, init: {}, **parameters
-			@crawl_api = crawl_api
+		def initialize crawlbot_api: nil, name: nil, init: {}, **parameters
+			@crawlbot_api = crawlbot_api
 			@name = name
 		 	if init.empty?
 				send_to_api parameters
@@ -26,7 +26,7 @@ module DiffbotSimple::V2
 			send_to_api parameters
 		end
 		def results
-			crawl_api.results url: parameters[:downloadJson]
+			crawlbot_api.results url: parameters[:downloadJson]
 		end
 		def method_missing property, *args
 			property = property.to_s.gsub(/\=$/,"").to_sym
@@ -34,10 +34,10 @@ module DiffbotSimple::V2
 			send_to_api({ property => args.join(",") })
 		end
 		private
-		attr_reader :crawl_api
+		attr_reader :crawlbot_api
 		def send_to_api **options
 			params = options.merge({name: name})
-			@parameters = crawl_api.single params
+			@parameters = crawlbot_api.single params
 			@parameters.delete :name
 			self
 		end
--- spec/v2/crawl_spec.rb
+++ spec/v2/crawl_spec.rb
@@ -1,53 +1,53 @@
 require 'spec_helper'
 module DiffbotSimple::V2
 	describe Crawl do
-		let(:crawl_api) { double("crawl_api") }
+		let(:crawlbot_api) { double("crawlbot_api") }
 		let(:name) { "test" }
-		let(:subject) { Crawl.new crawl_api: crawl_api, name: name}
-		before(:each) { expect(crawl_api).to receive(:single).with(name: name).and_return({ notifyEmail: nil, downloadJson: "download_url" }) }
+		let(:subject) { Crawl.new crawlbot_api: crawlbot_api, name: name}
+		before(:each) { expect(crawlbot_api).to receive(:single).with(name: name).and_return({ notifyEmail: nil, downloadJson: "download_url" }) }
 		context "when pausing" do
 			it "should send pause = 1 to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, pause: 1).and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, pause: 1).and_return({})
 				subject.pause
 			end
 		end
 		context "when unpausing" do
 			it "should send pause = 0 to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, pause: 0).and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, pause: 0).and_return({})
 				subject.unpause
 			end
 		end
 		context "when deleting" do
 			it "should send delete = 1 to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, delete: 1).and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, delete: 1).and_return({})
 				subject.delete!
 			end
 		end
 		context "when restarting" do
 			it "should send restart = 1 to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, restart: 1).and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, restart: 1).and_return({})
 				subject.restart
 			end
 		end
  		context "when downloading results" do
 			it "should call for results on crawl api" do
-				expect(crawl_api).to receive(:results).with(url: "download_url").and_return([])
+				expect(crawlbot_api).to receive(:results).with(url: "download_url").and_return([])
 				subject.results
 			end
 		end
 		context "when updating general parameters" do
 			it "should send these to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
 				subject.notifyEmail "foo@b.ar"
 			end
 			it "should even do it as an setter" do
-				expect(crawl_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar").and_return({})
 				subject.notifyEmail = "foo@b.ar"
 			end
 		end
 		context "when updating several properties at once" do
 			it "should send them to crawl api" do
-				expect(crawl_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar", repeat: 7.0).and_return({})
+				expect(crawlbot_api).to receive(:single).with(name: name, notifyEmail: "foo@b.ar", repeat: 7.0).and_return({})
 				subject.update notifyEmail: "foo@b.ar", repeat: 7.0
 			end
 		end
