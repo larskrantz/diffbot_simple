ac13df7c773c9f498655d86b4cf2f2a9ca2f17a3
Lars Krantz <lars.krantz@alaz.se>
2014-03-05 16:50:13 +0100

    Add: crawl-factory in client

--- lib/diffbot_simple/v2/client.rb
+++ lib/diffbot_simple/v2/client.rb
@@ -1,14 +1,12 @@
 require_relative 'api_client'
 module DiffbotSimple::V2
 	class Client
-		def initialize token: nil, bulk_api: nil, api_client: nil
+		def initialize token: nil, bulk_api: nil, api_client: nil, crawlbot_api: nil
 			raise ArgumentError.new("Must supply developer token") if token.to_s.empty?
 			@token = token
 			@api_client = api_client ||= ApiClient.new
 			@bulk_api = bulk_api ||= BulkApi.new(api_client: api_client, token: token)
-		end
-		def crawlbot
-			Crawlbot.new api_client: api_client, token: token
+			@crawlbot_api = crawlbot_api ||= CrawlbotApi.new(api_client: api_client, token: token)
 		end
 		def article
 			Article.new api_client: api_client, token: token
@@ -29,7 +27,11 @@ module DiffbotSimple::V2
 			return bulk_api.all.map { |e| Bulk.new name: e.delete(:name), init: e, bulk_api: bulk_api  } unless name
 			return Bulk.new name: name, bulk_api: bulk_api
 		end
+		def crawl name: nil
+			return crawlbot_api.all.map { |e| Crawl.new name: e.delete(:name), init: e, crawlbot_api: crawlbot_api  } unless name
+			return Crawl.new name: name, crawlbot_api: crawlbot_api
+		end
 		private
-		attr_reader :token, :api_client, :bulk_api
+		attr_reader :token, :api_client, :bulk_api, :crawlbot_api
 	end
 end
