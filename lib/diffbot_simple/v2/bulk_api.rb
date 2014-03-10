5107ad55693fadb6cd75bdc107c715ab2d5ab7cf
Lars Krantz <lars.krantz@alaz.se>
2014-03-10 13:33:33 +0100

    Refactor: bulk_api is a crawl_api and inheriths from that

--- lib/diffbot_simple/v2/bulk_api.rb
+++ lib/diffbot_simple/v2/bulk_api.rb
@@ -1,22 +1,8 @@
 module DiffbotSimple::V2
 	# Complies to http://www.diffbot.com/dev/docs/bulk/
-	class BulkApi
-		include ApiHelper
+	class BulkApi < CrawlbotApi
 		def post_initialize
 			@api = :bulk
-		end
-		def all
-			execute_call()[:jobs].select { |e| e[:type] == @api.to_s  }
-		end
-		def single name: nil, **options
-			response = execute_call options.merge(name: name)
-			return response[:jobs].select { |e| e[:type] == @api.to_s }.first if response.has_key?(:jobs)
-			response
-		end
-		def results url: nil
-			return [] unless url
-			response = api_client.get url
-			symbolize response
 		end
 	end
 end
