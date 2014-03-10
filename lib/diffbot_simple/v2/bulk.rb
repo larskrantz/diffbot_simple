5ed3c0c9b311840b28395d93560e1e6f7bd72fcd
Lars Krantz <lars.krantz@alaz.se>
2014-03-10 13:36:52 +0100

    Refactor: Bulk is based on Crawl, so now inherits therefrom

--- lib/diffbot_simple/v2/bulk.rb
+++ lib/diffbot_simple/v2/bulk.rb
@@ -1,47 +1,11 @@
 module DiffbotSimple::V2
-	class Bulk
-		attr_reader :parameters, :name
+	class Bulk < Crawl
 		def initialize bulk_api: nil, name: nil, init: {}, **parameters
-			@bulk_api = bulk_api
-			@name = name
-		 	if init.empty?
-				send_to_api parameters
-			else
-				@parameters = init
-			end
-		end
-		def pause
-			send_to_api pause: 1
-		end
-		def unpause
-			send_to_api pause: 0
-		end
-		def delete!
-			send_to_api delete: 1
-			@parameters = {}
-		end
-		def update **parameters
-			send_to_api parameters
+			super parameters.merge(name: name, crawlbot_api: bulk_api, init: init)
 		end
 		def process urls_to_process
 			urls_to_process = [urls_to_process] unless urls_to_process.respond_to? :join
 			send_to_api urls: urls_to_process.join(" ")
-		end
-		def results
-			bulk_api.results url: parameters[:downloadJson]
-		end
-		def method_missing property, *args
-			property = property.to_s.gsub(/\=$/,"").to_sym
-			super unless parameters.has_key? property
-			send_to_api({ property => args.join(",") })
-		end
-		private
-		attr_reader :bulk_api
-		def send_to_api **options
-			params = options.merge({name: name})
-			@parameters = bulk_api.single params
-			@parameters.delete :name
-			self
 		end
 	end
 end
